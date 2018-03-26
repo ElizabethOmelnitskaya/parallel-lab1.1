8e255c4d09a4e250f81dfca71e4aef8526281fcd
ElizabethOmelnitskaya <omelnitskaya.liza@gmail.com>
2018-03-26 20:24:23 +0300

    2
    2

--- client.py
+++ client.py
@@ -1,8 +1,18 @@
-import socket
+from settings import *
+from asyncio import open_connection, get_event_loop
+from collections import defaultdict
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 8887))
-s.send(b'Hello, Elizabeth!')
-data = s.recv(1024)
-s.close()
-print(data.decode())
+async def mapper(loop):
+    reader, writer = await open_connection(ADDR, PORT, loop=loop)
+    data = await reader.readuntil()
+    result = defaultdict(int)
+    with open(data.decode().rstrip(), "r") as f:
+        for line in f:
+            for word in line.rstrip().split():
+                result[word.lower()] += 1
+    writer.write(f'{dumps(result[WORD])}\n'.encode())
+    await writer.drain()
+    writer.close()
+
+loop = get_event_loop()
+loop.run_until_complete(mapper(loop)) and loop.close()
