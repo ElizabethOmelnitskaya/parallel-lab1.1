9ea674d90cb6477fccc067c4f736384b047f9d09
ElizabethOmelnitskaya <omelnitskaya.liza@gmail.com>
2018-03-26 20:33:14 +0300

    2
    2

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
-import socket
+from settings import *
+from asyncio import get_event_loop, Queue, start_server
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 8887))
-s.listen(1)
-conn, addr = s.accept()
-while True:
-    data = conn.recv(1024)
-    if not data: break
-    conn.send(data.upper())
-conn.close()
+processed, result, files = Queue(len(FILES)), 0, Queue()
+loop = get_event_loop()
+[files.put_nowait(f) for f in FILES]
+
+async def process_a_new_connection (reader, writer):
+    filename = await files.get()
+    writer.write(filename.encode())
+
+    await writer.drain()
+    res = await reader.readuntil()
+    writer.close()
+
+    global result
+    result += int(res.decode().rstrip())
+
+    await processed.put(filename)
+    if processed.full():
+        print(f'word <<{WORD}>> appeared {result} times')
+        loop.stop()
+
+coro = start_server(process_a_new_connection, ADDR, PORT, loop=loop)
+server = loop.run_until_complete(coro)
+
+try:
+    loop.run_forever()
+finally:
+    server.close()
